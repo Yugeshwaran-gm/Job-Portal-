8a0451f4da8b2d6f96b2bd7600854cd4214016a3
Yugeshwaran-gm <yugeshwarangm@gmail.com>
2025-02-18 11:36:33 +0530

    changes done in api

--- models/Notification.js
+++
@@ -1,9 +0,0 @@
-const NotificationSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    message: { type: String, required: true },
-    seen: { type: Boolean, default: false },
-    createdAt: { type: Date, default: Date.now }
-  });
-  
-  export default mongoose.model('Notification', NotificationSchema);
-  
